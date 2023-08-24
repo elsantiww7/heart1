e30d1d7092c9f5167ed7f9e97af1c735b1e69ea0
elsantiww7 <143103822+elsantiww7@users.noreply.github.com>
2023-08-24 09:26:29 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+for (let index = 0; index < 1e+58; index++) {
+    continue;
+}
+music.play(music.stringPlayable("A F E F D G E F ", 120), music.PlaybackMode.InBackground)
+basic.showIcon(IconNames.Target)
